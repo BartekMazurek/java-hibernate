96b8fc3312df4244fb70a2c07d13787514e0a9c0
bartek <bartlomiejmazurek9@gmail.com>
2023-09-18 18:22:19 +0200

    Added product model class

---
+++ src/main/java/org/example/model/Product.java
@@ -0,0 +1,45 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private int price;
+
+    public Product() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
